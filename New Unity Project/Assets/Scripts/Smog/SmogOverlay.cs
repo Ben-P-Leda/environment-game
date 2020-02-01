a966477e825821ff7a90739afdb6caf485243902
Ben-P-Leda <benp@ledaentertainment.co.uk>
2020-02-01 10:24:35 +0000

    Minor animation fix for bug, add smog cloud

---
+++ New Unity Project/Assets/Scripts/Smog/SmogOverlay.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Smog
+{
+    public class SmogOverlay : MonoBehaviour
+    {
+        private ParticleSystem _particles;
+
+        private float _particleDensity;
+
+        private void Awake()
+        {
+            _particles = GetComponent<ParticleSystem>();
+            _particleDensity = 0.0f;
+
+            SetSmogDensity();
+        }
+
+        private void SetSmogDensity()
+        {
+            ParticleSystem.MainModule particleMainSettings = _particles.main;
+            particleMainSettings.startColor = Color.Lerp(Color.clear, Color.gray, _particleDensity);
+
+            ParticleSystem.EmissionModule particleEmissionSettings = _particles.emission;
+            particleEmissionSettings.rateOverTime = new ParticleSystem.MinMaxCurve(Mathf.Lerp(0.0f, 100.0f, _particleDensity));
+        }
+
+        private void FixedUpdate()
+        {
+            _particleDensity = Mathf.Clamp01(_particleDensity + (Time.fixedDeltaTime * 0.1f));
+
+            SetSmogDensity();
+        }
+    }
+}
