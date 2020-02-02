5162af7d358bb3632c1ad7d5e3f8849977d139fd
Ben-P-Leda <benp@ledaentertainment.co.uk>
2020-02-02 16:16:01 +0000

    Add music

--- New Unity Project/Assets/Scripts/Game Management/GameController.cs
+++ New Unity Project/Assets/Scripts/Game Management/GameController.cs
@@ -12,6 +12,9 @@ namespace GameManagement
     {
         private SmogOverlay _smogOverlay;
         private List<ISuspendOnSmogLimitReached> _scriptsToSuspendWhenGameEnds = new List<ISuspendOnSmogLimitReached>();
+
+        private AudioSource _music;
+
         private bool _gameOver;
         private float _exitBlockTimer;
 
@@ -25,6 +28,9 @@ namespace GameManagement
 
         private void Awake()
         {
+            _music = FindObjectOfType<AudioSource>();
+            _music.volume = 1.0f;
+
             FindObjectOfType<SmogOverlay>().SmogCleared += HandleGameEnded;
             FindObjectOfType<PlantPool>().AllPlantsHaveDied += HandleGameEnded;
         }
@@ -47,12 +53,16 @@ namespace GameManagement
 
         private void FixedUpdate()
         {
-            if ((_gameOver) && (_exitBlockTimer <= 0.0f) && ((Input.GetButtonDown("P1:Action")) || (Input.GetButtonDown("P2:Action"))))
+            if (_gameOver)
             {
-                SceneManager.LoadScene("Start Scene");
-            }
+                if ((_exitBlockTimer <= 0.0f) && ((Input.GetButtonDown("P1:Action")) || (Input.GetButtonDown("P2:Action"))))
+                {
+                    SceneManager.LoadScene("Start Scene");
+                }
 
-            _exitBlockTimer = Mathf.Max(0.0f, _exitBlockTimer - Time.fixedDeltaTime);
+                _exitBlockTimer = Mathf.Max(0.0f, _exitBlockTimer - Time.fixedDeltaTime);
+                _music.volume = Mathf.Clamp01(_music.volume - Time.fixedDeltaTime * 2.0f);
+            }
         }
     }
 }
